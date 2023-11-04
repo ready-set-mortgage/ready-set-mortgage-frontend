393a0d3bb2e50948b52a9864be1cfdd64661836b
Emilio Marcos <emilio7marcos@gmail.com>
2023-11-04 18:05:19 -0500

    basic dashboard circles

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,15 +1,16 @@
-import { CircularProgressbar, buildStyles } from "react-circular-progressbar"
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "./Dashboard.css"
 
 export default function Dashboard() {
   return (
     <div className="dashboard">
       <div style={{ width: 150, height: 150 }}>
         <CircularProgressbar
-          value={70}
-          text="70"
+          value={55}
+          text="55"
           styles={buildStyles({
-            pathColor: "#770ef8",
-            textColor: "#770ef8",
+            pathColor: "#f80e1a",
+            textColor: "#f80e1a",
           })}
         />
       </div>
@@ -18,18 +19,18 @@ export default function Dashboard() {
           value={70}
           text="70"
           styles={buildStyles({
-            pathColor: "#770ef8",
-            textColor: "#770ef8",
+            pathColor: "#f8ec0e",
+            textColor: "#f8ec0e",
           })}
         />
       </div>
       <div style={{ width: 150, height: 150 }}>
         <CircularProgressbar
-          value={70}
-          text="70"
+          value={85}
+          text="85"
           styles={buildStyles({
-            pathColor: "#770ef8",
-            textColor: "#770ef8",
+            pathColor: "#1af80e",
+            textColor: "#1af80e",
           })}
         />
       </div>
