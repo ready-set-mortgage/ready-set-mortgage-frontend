c89fb55a3a7b0033cf28eed65b6ee17cddcfadcd
Emilio Marcos <emilio7marcos@gmail.com>
2023-11-05 10:58:23 -0600

    basic assessment shows

--- src/Components/Assessment/Assessment.jsx
+++ src/Components/Assessment/Assessment.jsx
@@ -1,11 +1,20 @@
+/* eslint-disable react/prop-types */
 import "./Assessment.css";
 
-// eslint-disable-next-line react/prop-types
 export default function Assessment({ output }) {
 
-  console.log(output);
+  const { readiness, breakdown, notices } = output;
+  const { credit, dti, fedti, ltv } = breakdown;
+  const messages = notices.map(notice => <p key={notice}>{notice}</p>)
 
   return (
-    <h1>Assessment</h1>
+    <>
+      <h2>{readiness}</h2>
+      {messages}
+      {credit.individual_readiness}
+      {dti.individual_readiness}
+      {fedti.individual_readiness}
+      {ltv.individual_readiness}
+    </>
   )
 }
