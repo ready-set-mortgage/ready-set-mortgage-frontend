a3ca98401a8b964b0967c314b4b7d662c33a9d34
Emilio Marcos <emilio7marcos@gmail.com>
2023-11-04 19:04:13 -0500

    display progressbar components

--- src/Components/Progressbar/Progressbar.jsx
+++ src/Components/Progressbar/Progressbar.jsx
@@ -1,5 +1,20 @@
-export default function Progressbar() {
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+
+// eslint-disable-next-line react/prop-types
+export default function Progressbar({ value }) {
+
+  const color = value > 74 ? "#1af80e" : value > 59 ? "#f8ec0e" : "#f80e1a";
+
   return (
-    <h1>Progressbar</h1>
+    <div style={{ width: 150, height: 150 }}>
+      <CircularProgressbar
+        value={value}
+        text={`${value}`}
+        styles={buildStyles({
+          pathColor: color,
+          textColor: color,
+        })}
+      />
+    </div>
   )
 }
