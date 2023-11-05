ad011db39b453b77907b49f9040cca08feee6336
Emilio Marcos <emilio7marcos@gmail.com>
2023-11-04 23:05:20 -0500

    restyle navbar logo

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import "./Navbar.css"
 export default function Navbar() {
   return (
     <nav>
-      <Link to="/"><div className="navbar-logo"><img className="navbar-icon" src={logo} alt="ReadySetMortgage logo" /><h2>ReadySetMortgage</h2></div></Link>
+      <Link to="/"><div className="navbar-logo"><img className="navbar-icon" src={logo} alt="rocket icon" /><h2>Ready, set, mortgage!</h2></div></Link>
       <Link to="/dashboard">Dashboard</Link>
     </nav>
   )
