88759150b81906842c36a5e54b827608bcd2d005
Emilio Marcos <emilio7marcos@gmail.com>
2023-11-04 21:54:35 -0500

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { Navbar, Dashboard } from './Components';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Navbar, Dashboard, Landing } from './Components';
 import './App.css';
 
 export default function App() {
 
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
       <Dashboard />
-    </>
+    </BrowserRouter>
   )
 }
