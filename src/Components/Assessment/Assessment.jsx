5101992c15a9b1e4a27d7299a8d67914be1fdf2c
Emilio Marcos <emilio7marcos@gmail.com>
2023-11-05 11:49:04 -0600

    last current styling

--- src/Components/Assessment/Assessment.jsx
+++ src/Components/Assessment/Assessment.jsx
@@ -8,20 +8,20 @@ export default function Assessment({ output }) {
   const { credit, dti, fedti, ltv } = breakdown;
   const messages = notices.map(notice => <p key={notice}>{notice}</p>)
 
-  const scores = {
-    credit: {
+  // const scores = {
+  //   credit: {
 
-    }
-  }
+  //   }
+  // }
 
   return (
     <>
       <h2>{readiness == "N" ? "You are not likely eligible for the mortgage." : readiness == "M" ? "You might be eligible for the mortgage." : "You are very likely eligible for the mortgage."}</h2>
       {messages}
-      <h5>Credit:   {credit.individual_readiness}</h5>
-      <h5>DTI:   {dti.individual_readiness}</h5>
-      <h5>FEDTI:   {fedti.individual_readiness}</h5>
-      <h5>LTV:   {ltv.individual_readiness}</h5>
+      <div className="score-container"><h5>Credit:   {credit.individual_readiness == "Y" ? <Progressbar value={85} /> : <Progressbar value={59} />  }</h5></div>
+      <div className="score-container"><h5>DTI:   {dti.individual_readiness == "Y" ? <Progressbar value={85} /> : <Progressbar value={59} />  }</h5></div>
+      <div className="score-container"><h5>FEDTI:   {fedti.individual_readiness == "Y" ? <Progressbar value={85} /> : <Progressbar value={59} />  }</h5></div>
+      <div className="score-container"><h5>LTV:   {ltv.individual_readiness == "Y" ? <Progressbar value={85} /> : <Progressbar value={59} />  }</h5></div>
     </>
   )
 }
