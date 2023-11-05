dcb09563c3394e07be1f1803535aaa1e1e1c27d4
Emilio Marcos <emilio7marcos@gmail.com>
2023-11-05 09:54:54 -0600

    create assessment component

---
+++ src/Components/Assessment/Assessment.jsx
@@ -0,0 +1,7 @@
+export default function Assessment() {
+  return (
+    <h1>
+      Assessment
+    </h1>
+  )
+}
--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -3,7 +3,7 @@ import "./Form.css";
 
 export default function Form() {
 
-  const [assessment, setAssessment] = useState("");
+  const [assessment, setAssessment] = useState({});
   const [formInput, setFormInput] = useState({
     gross_monthly_income: "",
     monthly_car_payment: "",
@@ -47,22 +47,24 @@ export default function Form() {
   }
 
   return (
-    <div className="form-container">
-      <form onSubmit={handleSubmit}>
-        <h3 className="form-title">Mortgage Eligibility Assessment</h3>
-        <div className="form">
-          <input name="gross_monthly_income" value={formInput.gross_monthly_income} type="text" placeholder="Gross monthly income" onChange={handleInputChange}/>
-          <input name="monthly_car_payment" value={formInput.monthly_car_payment} type="text" placeholder="Monthly car payment" onChange={handleInputChange}/>
-          <input name="monthly_credit_card_payment" value={formInput.monthly_credit_card_payment} type="text" placeholder="Monthly credit card payment" onChange={handleInputChange}/>
-          <input name="student_loan_payment" value={formInput.student_loan_payment} type="text" placeholder="Student loan payment" onChange={handleInputChange}/>
-          <input name="home_appraised_value" value={formInput.home_appraised_value} type="text" placeholder="Home appraised value" onChange={handleInputChange}/>
-          <input name="est_monthly_mortgage_payment" value={formInput.est_monthly_mortgage_payment} type="text" placeholder="Estimated monthly mortgage payment" onChange={handleInputChange}/>
-          <input name="down_payment_amount" value={formInput.down_payment_amount} type="text" placeholder="Down payment amount" onChange={handleInputChange}/>
-          <input name="credit_score" value={formInput.credit_score} type="text" placeholder="Credit score" onChange={handleInputChange}/>
-        </div>
-        <button className="form-button">Submit</button>
-      </form>
-      {/* {assessment ? assessment : null} */}
-    </div>
+    <>
+      <div className="form-container">
+        <form onSubmit={handleSubmit}>
+          <h3 className="form-title">Mortgage Eligibility Assessment</h3>
+          <div className="form">
+            <input name="gross_monthly_income" value={formInput.gross_monthly_income} type="text" placeholder="Gross monthly income" onChange={handleInputChange}/>
+            <input name="monthly_car_payment" value={formInput.monthly_car_payment} type="text" placeholder="Monthly car payment" onChange={handleInputChange}/>
+            <input name="monthly_credit_card_payment" value={formInput.monthly_credit_card_payment} type="text" placeholder="Monthly credit card payment" onChange={handleInputChange}/>
+            <input name="student_loan_payment" value={formInput.student_loan_payment} type="text" placeholder="Student loan payment" onChange={handleInputChange}/>
+            <input name="home_appraised_value" value={formInput.home_appraised_value} type="text" placeholder="Home appraised value" onChange={handleInputChange}/>
+            <input name="est_monthly_mortgage_payment" value={formInput.est_monthly_mortgage_payment} type="text" placeholder="Estimated monthly mortgage payment" onChange={handleInputChange}/>
+            <input name="down_payment_amount" value={formInput.down_payment_amount} type="text" placeholder="Down payment amount" onChange={handleInputChange}/>
+            <input name="credit_score" value={formInput.credit_score} type="text" placeholder="Credit score" onChange={handleInputChange}/>
+          </div>
+          <button className="form-button">Submit</button>
+        </form>
+      </div>
+      {assessment ? assessment.readiness : null}
+    </>
   )
 }
