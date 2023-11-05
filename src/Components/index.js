1b7177e8864aeb79c796802ba649083246c719a3
Emilio Marcos <emilio7marcos@gmail.com>
2023-11-05 09:55:35 -0600

    export default components

--- src/Components/index.js
+++ src/Components/index.js
@@ -2,3 +2,5 @@ export { default as Navbar } from './Navbar/Navbar';
 export { default as Dashboard } from './Dashboard/Dashboard';
 export { default as Landing } from './Landing/Landing';
 export { default as Form } from './Form/Form';
+export { default as Assessment } from './Assessment/Assessment';
+export { default as Progressbar } from './Progressbar/Progressbar';
