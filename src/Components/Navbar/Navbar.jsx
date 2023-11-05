2acd017d47f553f2c8290f7f0451f431ab7cf0ac
Emilio Marcos <emilio7marcos@gmail.com>
2023-11-04 22:36:00 -0500

    styling new logo and colors

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
-import logo from "../../Assets/logo.png";
+import logo from "../../Assets/logo.svg";
 import "./Navbar.css"
 
 export default function Navbar() {
   return (
     <nav>
-      <Link to="/"><img className="navbar-logo" src={logo} alt="ReadySetMortgage logo" /></Link>
+      <Link to="/"><div className="navbar-logo"><img className="navbar-icon" src={logo} alt="ReadySetMortgage logo" /><h2>ReadySetMortgage</h2></div></Link>
       <Link to="/dashboard">Dashboard</Link>
     </nav>
   )
