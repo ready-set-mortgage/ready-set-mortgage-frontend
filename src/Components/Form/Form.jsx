d209c4b9d3ebeeeca0610a929d3742c1339f5c25
Emilio Marcos <emilio7marcos@gmail.com>
2023-11-05 01:12:31 -0500

    basic form setup

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,9 +1,23 @@
 import "./Form.css";
 
 export default function Form() {
+
+  function handleSubmit() {
+    console.log("form submitted")
+  }
+
   return (
     <div className="form-container">
-
+      <form onSubmit={handleSubmit}>
+        <h3>Apply to Buy a Home</h3>
+        <div className="form">
+          <input type="text" placeholder="info 1" />
+          <input type="text" placeholder="info 2" />
+          <input type="text" placeholder="info 3" />
+          <input type="text" placeholder="info 4" />
+        </div>
+        <button>Submit</button>
+      </form>
     </div>
   )
 }
--- src/Components/index.js
+++ src/Components/index.js
@@ -1,3 +1,4 @@
 export { default as Navbar } from './Navbar/Navbar';
 export { default as Dashboard } from './Dashboard/Dashboard';
 export { default as Landing } from './Landing/Landing';
+export { default as Form } from './Form/Form';
